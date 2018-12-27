cbea0264f2087e30efb776dd33c85810f769eb0d
Lee <lhc9138@naver.com>
2018-12-27 15:22:34 +0900

    Add method signature - DAO

--- src/com/persistance/AdminDAO.java
+++ src/com/persistance/AdminDAO.java
@@ -1,5 +1,14 @@
 package com.persistance;
 
+import java.util.List;
+
+import com.domain.Admin;
+
 public interface AdminDAO {
 
+	// (출력) 강사 - 개인 설정 - 개인정보 확인
+	public List<Admin> info();
+		
+	// (수정) 관리자 - 개인 설정 - 비밀번호 변경
+	public int changepw(Admin admin);
 }
