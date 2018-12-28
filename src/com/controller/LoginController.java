dbe428eb3d66744ca8da4467d8c7220ea30455b7
Lee <lhc9138@naver.com>
2018-12-28 15:15:00 +0900

    Create LoginController.java File

--- src/com/controller/LoginController.java
+++ src/com/controller/LoginController.java
@@ -1,5 +1,50 @@
 package com.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.service.AdminService;
+import com.service.InstructorService;
+import com.service.StudentService;
+
+@Controller
 public class LoginController {
 
+	@Resource(name = "studentService")
+	private StudentService studentService;
+
+	@Resource(name = "instructorService")
+	private InstructorService instructorService;
+
+	@Resource(name = "adminService")
+	private AdminService adminService;
+	   
+	@RequestMapping("/index")
+	public String index() {
+		
+		//WEB-INF/views/login/loginForm.jsp
+		return "common/common_login";
+	}
+	
+	@RequestMapping("/common/login")
+	public String login(HttpSession session) {
+		
+		return null;
+	}
+	
+	@RequestMapping("/common/loginFail")
+	public String loginFail() {
+		
+		return "common/common_loginfailform";
+	}
+	
+	@RequestMapping("/common/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		
+		return "common/common_logoutform";
+	}
 }
