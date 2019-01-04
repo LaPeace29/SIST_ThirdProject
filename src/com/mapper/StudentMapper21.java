37046421ee701d72acc733c7c314f3eb41a6d908
lhc9138 <lhc9138@naver.com>
2019-01-05 02:11:39 +0900

    Create Or Replace Mapper

--- src/com/mapper/StudentMapper21.java
+++ src/com/mapper/StudentMapper21.java
@@ -7,16 +7,20 @@ import org.springframework.jdbc.core.RowMapper;
 
 import com.domain.Student;
 
-public class StudentMapper21 implements RowMapper<Student> {
+public class StudentMapper21 implements RowMapper<Student>  {
 
 	@Override
 	public Student mapRow(ResultSet rs, int count) throws SQLException {
 		
-		// TODO Auto-generated method stub
 		Student st = new Student();
+		
 		st.setStudent_id(rs.getString("student_id"));
+		st.setStudent_name(rs.getString("student_name"));
+		st.setStudent_phone(rs.getString("student_phone"));
+		st.setStudent_regDate(rs.getDate("student_regDate"));
+		
 		
 		return st;
 	}
-	
+
 }
