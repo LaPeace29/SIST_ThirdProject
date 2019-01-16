b080bb6f02cf1087eb168898034d8b64376e634a
lhc9138 <lhc9138@naver.com>
2019-01-17 03:58:04 +0900

    Modify Mapper Files

--- src/com/mapper/ExamMapper21.java
+++ src/com/mapper/ExamMapper21.java
@@ -12,31 +12,25 @@ public class ExamMapper21 implements RowMapper<Exam> {
 	@Override
 	public Exam mapRow(ResultSet rs, int count) throws SQLException {
 		
-		// �����ͺ��̽����� �о�� �����
-		// �̸� �غ��� ��ü�� Ư�� ������ ����
 		Exam exam = new Exam();
 		
-
-		
 		exam.setSubject_name(rs.getString("subject_name"));
-		exam.setSubjectbook_name(rs.getString("subjectbook_name"));
-		exam.setInstructor_name(rs.getString("instructor_name"));
-		exam.setExam_date(rs.getDate("exam_date"));
-		exam.setAttendance_score(rs.getInt("attendance_score"));
-		exam.setAttendance_point(rs.getInt("attendance_point"));
-		exam.setWrite_score(rs.getInt("write_score"));
-		exam.setWrite_point(rs.getInt("write_point"));
-		exam.setSkill_score(rs.getInt("skill_score"));
-		exam.setSkill_point(rs.getInt("skill_point"));
-		exam.setExam_file(rs.getString("exam_file"));
-		exam.setSubjectbook_isbn(rs.getString("subjectbook_isbn"));
-		exam.setInstructor_photo_filepath(rs.getString("instructor_photo_filepath"));
 		exam.setSubject_start_date(rs.getDate("subject_start_date"));
 		exam.setSubject_end_date(rs.getDate("subject_end_date"));
-		
-		
-		
+		exam.setInstructor_name(rs.getString("instructor_name"));
+		exam.setInstructor_phone(rs.getString("instructor_phone"));
+		exam.setInstructor_photo_filepath(rs.getString("instructor_photo_filepath"));
+		exam.setSubjectbook_name(rs.getString("subjectbook_name"));
+		exam.setSubjectbook_isbn(rs.getString("subjectbook_isbn"));
+		exam.setExam_date(rs.getDate("exam_date"));
+		exam.setExam_file(rs.getString("exam_file"));
+		exam.setAttendance_point(rs.getInt("attendance_point"));
+		exam.setWrite_point(rs.getInt("write_point"));
+		exam.setSkill_point(rs.getInt("skill_point"));
+		exam.setAttendance_score(rs.getInt("attendance_score"));
+		exam.setWrite_score(rs.getInt("write_score"));
+		exam.setSkill_score(rs.getInt("skill_score"));
+
 		return exam;
 	}
-
 }
--- src/com/mapper/OpenSubjectMapper42.java
+++ src/com/mapper/OpenSubjectMapper42.java
@@ -7,24 +7,22 @@ import org.springframework.jdbc.core.RowMapper;
 
 import com.domain.OpenSubject;
 
-public class OpenSubjectMapper2 implements RowMapper<OpenSubject> {
+public class OpenSubjectMapper42 implements RowMapper<OpenSubject> {
 
 	@Override
 	public OpenSubject mapRow(ResultSet rs, int count) throws SQLException {
 		
-		// 데이터베이스에서 읽어온 결과를
-		// 미리 준비한 객체의 특정 변수와 연결
 		OpenSubject os = new OpenSubject();
 		
 		os.setOpen_subject_id(rs.getString("open_subject_id"));
 		os.setSubject_name(rs.getString("subject_name"));
-		os.setCourse_name(rs.getString("course_name"));
 		os.setSubject_start_date(rs.getDate("subject_start_date"));
 		os.setSubject_end_date(rs.getDate("subject_end_date"));
+		os.setCourse_name(rs.getString("course_name"));
 		os.setOpen_course_start_date(rs.getDate("open_course_start_date"));
 		os.setOpen_course_end_date(rs.getDate("open_course_end_date"));
 		os.setClassroom_name(rs.getString("classroom_name"));
-		os.setCompletion(rs.getString("completion"));
+		os.setInstructor_status(rs.getString("instructor_status"));
 		
 		return os;
 	}
